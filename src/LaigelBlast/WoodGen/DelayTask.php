2eb17edb4e6e0779a485a82dd86f2ced1404703e
BLAST1718 <71514704+BLAST1718@users.noreply.github.com>
2021-01-26 11:32:26 +0800

    Update DelayTask.php

--- src/LaigelBlast/WoodGen/DelayTask.php
+++ src/LaigelBlast/WoodGen/DelayTask.php
@@ -12,29 +12,17 @@ use pocketmine\scheduler\Task;
 
 
 
-class DelayTask extends Task {
-
-
+cclass DelayTask extends Task{
 
     public $block, $plugin;
 
-
-
-    public function __construct(Main $plugin, Block $block) {
-
+    public function __construct(Main $plugin, Block $block){
         $this->plugin = $plugin;
 
         $this->block = $block;
-
     }
 
-
-
-    public function onRun(int $currentTick) {
-
-        $this->block->getLevelNonNull()->setBlock($this->block->asVector3(), Block::get($this->block->getId()));
-
+    public function onRun(int $currentTick){
+        $this->block->getLevelNonNull()->setBlock($this->block->asVector3(), Block::get($this->block->getId(), $this->block->getDamage()));
     }
-
 }
-
