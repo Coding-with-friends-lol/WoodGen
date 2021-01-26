e8f799ed16bc1094db804905fea1e95bcfda0bbf
BLAST1718 <71514704+BLAST1718@users.noreply.github.com>
2021-01-26 09:14:39 +0800

    Update Main.php

--- src/LaigelBlast/WoodGen/Main.php
+++ src/LaigelBlast/WoodGen/Main.php
@@ -4,87 +4,85 @@ namespace LaigelBlast\WoodGen;
 
 use pocketmine\block\Block;
 use pocketmine\event\block\BlockBreakEvent;
-use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 
 class Main extends PluginBase implements Listener{
 
-    private $config;
+	private $config;
 
-    public function onEnable()
-    {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->saveResource("config.yml");
-        $this->config = new Config($this->getDataFolder()."config.yml", Config::YAML);
-    }
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->saveResource("config.yml");
+		$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+	}
 
-    public function onBlockBreak(BlockBreakEvent $event){
-        $player = $event->getPlayer();
-        $block = $event->getBlock();
-        if($block->getLevel()->getName() === $this->config->get("levelname")){
-            if($block->getId() === 17 && $this->config->get("oak_wood") === true){
-                foreach ($event->getDrops() as $drop) {
-                    $event->getPlayer()->getInventory()->addItem($drop);
-                }
-                $event->setDrops([]);
-                $event->setCancelled();
-                $event->setXpDropAmount(0);
-                $player->addXp($event->getXpDropAmount());
-                $block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(0));
-                $this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("ow_delay"));
-            } elseif( in_array($block->getId(), [17, 0, 1]) && $this->config->get("spruce_wood") === true){
-                foreach ($event->getDrops() as $drop) {
-                    $event->getPlayer()->getInventory()->addItem($drop);
-                }
-                $event->setDrops([]);
-                $event->setCancelled();
-                $event->setXpDropAmount(0);
-                $player->addXp($event->getXpDropAmount());
-                $block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
-                $this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("sw_delay"));
-           } elseif( in_array($block->getId(), [17, 0, 2]) && $this->config->get("birch_wood") === true){{
-                foreach ($event->getDrops() as $drop) {
-                    $event->getPlayer()->getInventory()->addItem($drop);
-                }
-                $event->setDrops([]);
-                $event->setCancelled();
-                $event->setXpDropAmount(0);
-                $player->addXp($event->getXpDropAmount());
-                $block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
-                $this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("bw_delay"));
-            } elseif( in_array($block->getId(), [17, 0, 3]) && $this->config->get("dark_oak_wood") === true){
-                foreach ($event->getDrops() as $drop) {
-                    $event->getPlayer()->getInventory()->addItem($drop);
-                }
-                $event->setDrops([]);
-                $player->addXp($event->getXpDropAmount());
-                $event->setCancelled();
-                $event->setXpDropAmount(0);
-                $block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
-                $this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("dow_delay"));
-            } elseif($block->getId() === Block::EMERALD_ORE && $this->config->get("acacia_wood") === true){
-                foreach ($event->getDrops() as $drop) {
-                    $event->getPlayer()->getInventory()->addItem($drop);
-                }
-                $event->setDrops([]);
-                $player->addXp($event->getXpDropAmount());
-                $event->setCancelled();
-                $event->setXpDropAmount(0);
-                $block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
-                $this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("aw_delay"));
-            } elseif($block->getId() === Block::QUARTZ_ORE && $this->config->get("jungle_wood") === true){
-                foreach ($event->getDrops() as $drop) {
-                    $event->getPlayer()->getInventory()->addItem($drop);
-                }
-                $event->setDrops([]);
-                $player->addXp($event->getXpDropAmount());
-                $event->setCancelled();
-                $event->setXpDropAmount(0);
-                $block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
-                $this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("jw_delay"));
-            }
-        }
-    }
-
+	public function onBlockBreak(BlockBreakEvent $event){
+		$player = $event->getPlayer();
+		$block = $event->getBlock();
+		if($block->getLevel()->getName() === $this->config->get("levelname")){
+			if($block->getId() === 17 && $this->config->get("oak_wood") === true){
+				foreach($event->getDrops() as $drop){
+					$event->getPlayer()->getInventory()->addItem($drop);
+				}
+				$event->setDrops([]);
+				$event->setCancelled();
+				$event->setXpDropAmount(0);
+				$player->addXp($event->getXpDropAmount());
+				$block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(0));
+				$this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("ow_delay"));
+			}elseif(in_array($block->getId(), [17, 0, 1]) && $this->config->get("spruce_wood") === true){
+				foreach($event->getDrops() as $drop){
+					$event->getPlayer()->getInventory()->addItem($drop);
+				}
+				$event->setDrops([]);
+				$event->setCancelled();
+				$event->setXpDropAmount(0);
+				$player->addXp($event->getXpDropAmount());
+				$block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
+				$this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("sw_delay"));
+			}elseif(in_array($block->getId(), [17, 0, 2]) && $this->config->get("birch_wood") === true){
+				foreach($event->getDrops() as $drop){
+					$event->getPlayer()->getInventory()->addItem($drop);
+				}
+				$event->setDrops([]);
+				$event->setCancelled();
+				$event->setXpDropAmount(0);
+				$player->addXp($event->getXpDropAmount());
+				$block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
+				$this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("bw_delay"));
+			}elseif(in_array($block->getId(), [17, 0, 3]) && $this->config->get("dark_oak_wood") === true){
+				foreach($event->getDrops() as $drop){
+					$event->getPlayer()->getInventory()->addItem($drop);
+				}
+				$event->setDrops([]);
+				$player->addXp($event->getXpDropAmount());
+				$event->setCancelled();
+				$event->setXpDropAmount(0);
+				$block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
+				$this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("dow_delay"));
+			}elseif($block->getId() === Block::EMERALD_ORE && $this->config->get("acacia_wood") === true){
+				foreach($event->getDrops() as $drop){
+					$event->getPlayer()->getInventory()->addItem($drop);
+				}
+				$event->setDrops([]);
+				$player->addXp($event->getXpDropAmount());
+				$event->setCancelled();
+				$event->setXpDropAmount(0);
+				$block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
+				$this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("aw_delay"));
+			}elseif($block->getId() === Block::QUARTZ_ORE && $this->config->get("jungle_wood") === true){
+				foreach($event->getDrops() as $drop){
+					$event->getPlayer()->getInventory()->addItem($drop);
+				}
+				$event->setDrops([]);
+				$player->addXp($event->getXpDropAmount());
+				$event->setCancelled();
+				$event->setXpDropAmount(0);
+				$block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
+				$this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("jw_delay"));
+			}
+		}
+	}
 }
